87a9de8a9d0438c0b769f255fd316d4245648d56
fatimaferdous <101489576+fatimaferdous@users.noreply.github.com>
2023-12-04 19:09:30 -0500

    Update patient.cpp

--- comp3004_project/patient.cpp
+++ comp3004_project/patient.cpp
@@ -1,6 +1,21 @@
-#include "patient.h"
+#include "Patient.h"
 
-patient::patient()
-{
+Patient::Patient(){}
 
+Patient::Patient(int age){
+    this->age = age;
+}
+
+bool Patient::isChild(){
+    if (age < 18){
+        return true;
+    }
+    return false;
+}
+
+bool Patient::isAdult(){
+    if (age > 17){
+        return true;
+    }
+    return false;
 }
