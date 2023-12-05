069eb3ad74e02f356e8c63cd5bc541cbb626e8f8
fatimaferdous <101489576+fatimaferdous@users.noreply.github.com>
2023-12-04 19:38:25 -0500

    Update patient.cpp

--- comp3004_project/patient.cpp
+++ comp3004_project/patient.cpp
@@ -2,19 +2,29 @@
 
 Patient::Patient(){}
 
-Patient::Patient(int age){
+Patient::Patient(int age, int weight){
     this->age = age;
+    this->weight = weight;
 }
 
 bool Patient::isChild(){
-    if (age < 18){
+    if (age < 9 || weight < 25){
         return true;
     }
     return false;
 }
 
 bool Patient::isAdult(){
-    if (age > 17){
+    if (age > 8 || weight > 25){
+        return true;
+    }
+    return false;
+}
+
+bool Patient::shockableHeartRate(){
+    if (this->isChild() && bpm > 200){
+        return true;
+    } else if (this->isAdult() && bpm > 150){
         return true;
     }
     return false;
