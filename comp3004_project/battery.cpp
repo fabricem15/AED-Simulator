defef550dfd6e8a7808bd9ee1f9543a9b8a8ac40
fatimaferdous <101489576+fatimaferdous@users.noreply.github.com>
2023-12-04 19:52:13 -0500

    Update battery.cpp

--- comp3004_project/battery.cpp
+++ comp3004_project/battery.cpp
@@ -1,6 +1,41 @@
-#include "battery.h"
+#include "Battery.h"
 
-battery::battery()
-{
-
+Battery::Battery(){
+    charge = 100;
+    
 }
+
+void Battery::decreaseBattery(){
+    if (electrode.heartAnalysis() && p.isChild()){
+        charge -= 5;
+    } else if (electrode.heartAnalysis() && p.isAdult()){
+        charge -= 10;
+    }
+    
+    if (charge <= 0){
+        charge = 0;
+    }
+}
+
+bool Battery::replaceBattery(){
+    if (charge < 20){
+        vp.resetBattery();
+        return true;
+    }
+    return false;
+}
+
+void Battery::resetBattery(){
+    if (this->replaceBattery()){
+        charge = 100;
+        qInfo("batteries replaced and charge is at 100%");
+    }
+}
+
+int Battery::getCharge(){
+    return charge;
+}
+
+/*void Battery::selfTest(){
+    
+}*/
